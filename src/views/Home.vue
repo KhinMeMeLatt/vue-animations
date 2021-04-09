8f5d704cea5e920bada1fe6ad4ca4bd9df24d9f1
Khin Me Me Latt <khinmemelatt@outlook.com>
2021-04-10 00:01:15 +0630

    bounce animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+      <button @click="bounce = !bounce">Bounce</button><br>
+      <transition name="bounce_ani">
+        <img v-if="bounce" alt="Vue logo" src="../assets/logo.png">
+      </transition>
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      bounce: false
+    }
   }
 }
 </script>
+
+<style scoped>
+section {
+  min-height: 200px;
+}
+.bounce_ani-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce_ani-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
